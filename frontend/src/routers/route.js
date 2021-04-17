ab88bd6645a0e8d575544c8d1d953ab385297d27
roeun <pacharothroeun01@gmail.com>
2021-04-17 17:42:01 +0700

    [Test] Route UI for testing component

--- frontend/src/routers/route.js
+++ frontend/src/routers/route.js
@@ -1,11 +1,36 @@
 import {createRouter,createWebHistory} from 'vue-router';
 import AdminLayout from '../components/layout/AdminLayout.vue';
+import edit_user from '../components/customer/edit_user.vue';
+import feedback from '../components/customer/feedback.vue';
+import shopping_cart from '../components/customer/shopping_cart.vue';
+import post_order from '../components/customer/post_order.vue';
 const routes = [
     {
         path:'/admin',
         name:'Admin',
         component:AdminLayout,
     },
+    {
+        path:'/edituser',
+        name:'Edit User',
+        component:edit_user,
+    },
+    {
+        path:'/feedback',
+        name:'FeedBack',
+        component:feedback,
+    },
+    {
+        path:'/postorder',
+        name:"PostOrder",
+        component:post_order,
+    },
+    {
+        path:'/shoppingcart',
+        name:'shopping cart',
+        component:shopping_cart,
+    }
+
 ]
 const router = createRouter({
     history:createWebHistory(process.env.BASE_URL),
