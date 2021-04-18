e31ee7b5297552b304865cab699199852c37d483
roeun <pacharothroeun01@gmail.com>
2021-04-18 16:28:56 +0700

    [WIP] Customer Phearun part - He cant push to his branch so we help him push the code

---
+++ frontend/src/components/Admin/CustomerAdmin.vue
@@ -0,0 +1,30 @@
+<template>
+      <div class="container">
+    <h2 class="text-secondary "><strong>Customer</strong></h2>
+    <div class="row">
+      <table id="example" class="table table-striped" style="width:100%">
+        <thead class="bg-primary text-light">
+            <th scope=""><input type="checkbox" onclick="checkAll(this)"></th>
+            <th scope="">Name</th>
+            <th scope="">Phone</th>
+            <th scope="">Email</th>
+            <th scope="">Register Date</th>
+            <th scope="">Adress</th>
+            <th scope="">Action</th>
+        </thead>
+        <CustomerAdminList v-for="postItem in postList" :key="postItem.id" :postItem="postItem"></CustomerAdminList>
+    </table>
+    </div>
+    
+  </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+    @import '../../assets/sass/colorpage';
+    @import '../../assets/sass/maxin';
+    @import'../../../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
---
+++ frontend/src/components/Admin/CustomerAdminList.vue
@@ -0,0 +1,19 @@
+<template>
+    <tbody>
+     <tr>
+        <td><input type="checkbox" name=""></td>
+        <th scope=""> {{postItem.name}}</th>
+        <td>{{postItem.phone}}</td>
+        <td>{{postItem.email}}</td>
+        <td>{{postItem.register}}</td>
+        <td>{{postItem.address}}</td>
+        <td><span data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn text-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><i class="far fa-edit"></i></button></span>
+            <span data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn text-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><i class="far fa-trash-alt"></i></button></span>
+        </td>
+    </tr>
+  </tbody>
+    
+</template>
+<style lang="scss">
+    
+</style>
