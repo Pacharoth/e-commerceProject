bc835a78e99ae3075be4431e68ed175f82cef614
Naro Kuoch <narokuoch@gmail.com>
2021-05-29 15:50:50 +0700

    list categories from data in home page

--- frontend/src/components/customer/Categorydropdown.vue
+++ frontend/src/components/customer/Categorydropdown.vue
@@ -6,17 +6,25 @@
         </button>
         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
             <h6 class="title ms-2">Category</h6>
-          <li><a class="dropdown-item" href="#">Electronic</a></li>
-          <li><a class="dropdown-item" href="#">Clothes</a></li>
-          <li><a class="dropdown-item" href="#">Bag</a></li>
-          <li><a class="dropdown-item" href="#">Computer</a></li>
+          <li v-for="cat in categories" :key="cat"><a class="dropdown-item" href="#">{{cat.name}}</a></li>
         </ul>
     </div>
     
 </template>
 <script>
+import axios from 'axios'
 export default {
     name:'Categorydropdown',
+    data(){
+        return{
+            categories:[],
+        }
+    },
+    async mounted(){
+        const res = await axios.get('http://localhost:3000/category')
+        console.log('result of category',res.data)
+        this.categories=res.data
+    }
 }
 </script>
 <style scoped>
