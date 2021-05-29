4cd586981ea115f74b6f4374f0eeba9db1dea4d8
roeun <pacharothroeun01@gmail.com>
2021-05-29 18:30:29 +0700

    [testing]

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,33 +1,7 @@
 const product = require('../models/productModel');
 const categoryModel = require('../models/categoryModel')
 const seller = require('../models/sellerModel')
-// exports.postProduct = async(req,res)=>{
-//     const reqs= req.body
-//     const arrayPic=[]
-//     const reqFile = req.files.img
-//     for(const i in reqFile){
-//         var filename='/assets/img/'+reqFile[i].name;
-//         var pathSave=__dirname+filename
-//         await reqFile[i].mv(pathSave)
-//         arrayPic.push(filename);
-//     }
-    
-//     const product = new product({
-//         categories:reqs.categoryid,
-//         sellers:reqs.sellerid,
-//         productName:reqs.productName,
-//         sizeOrcolor:null,
-//         description:reqs.description,
-//         discount:reqs.discount,
-//         quantity:reqs.quamtity,
-//         price:(reqs.price!==null||reqs.price!==undefined)?reqs.price:0,
-//         img:arrayPic,
-//     })
-//     await product.save()
-//     res.json(await product.populate('categories').populate('sellers').exec())
-// }
 exports.getProducts=async(req,res)=>{
-   
     const products = await product.find({sellers:req.params.id}).populate('categories')
     console.log('got from db', products)
     res.json(products);
@@ -45,21 +19,24 @@ exports.postProduct = async (req,res)=>{
             name:productname,
             price: price,
             instock:instock,
-            sellers:sellerid,
             qty:qty,
             detail:detail,
             img:filename
         })
         console.log(newProduct);
         const cate = await categoryModel.find({name:category});
-        newProduct.categories = cate[0]._id
+        const aseller = await seller.find().populate({
+            path:'users',
+            match:{
+                _id:sellerid
+            }
+        })
+        aseller = aseller.filter(element=>element.users!==null);
+        newProduct.sellers = aseller[0]._id;
+        newProduct.categories = cate[0]._id;
         await newProduct.save()
-
         res.json({'message':'New prodcut is created'})
     }else{
         res.json({'message':"Can't create product"})
     }
-    // console.log("file",file)
 }
-
-//http://localhost:3000/assets/pic.png
