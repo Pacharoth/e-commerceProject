dee6fe8b00473338ce54214755559d182add67de
Naro Kuoch <narokuoch@gmail.com>
2021-05-30 08:57:34 +0700

    modified frontend for uploading profile img

--- frontend/src/components/Seller/SellerProfile.vue
+++ frontend/src/components/Seller/SellerProfile.vue
@@ -19,14 +19,44 @@
             <div class="col-md-3">Address</div>
             <div class="col-md-8"><span class="colon">:</span>{{seller.address}}</div>
           </div>
-          <div class="row"><div class="col"><button class="btn" type="button">Change Password</button></div></div>
+          <div class="row">
+            <div class="col">
+              <button @click="modal.show()" class="btn pink" type="button">
+                Change Password
+              </button>
+            </div>
+          </div>
         </div>
        
         <div class="col-md-4 data">
           <center>
             <div>Profile Image</div>
             <img class="rounded mx-auto d-block" src="../../assets/img/profileSeller.jpg" alt="">
-            <div><button type="button" class="btn">Upload New</button></div>
+            <div>
+              <button type="button" class="btn pink" @click="modal.show()" data-toggle="modal">Upload New</button>
+            </div>
+            <!-- add img model -->
+            <div class="modal fade" id="example" tabindex="-1" ref="modal" role="dialog" aria-labelledby="exampleLabel" aria-hidden="false">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title header" id="exampleModalLabel">Add Profile Image</h5>
+                    <button type="button" class="close" @click="modal.hide()" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <form ref="form" enctype="multipart/form-data">
+                    <div class="modal-body">
+                      <input ref="img" class="form-control" type="file" id="profilepic" name="profiepic" />
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" @click="modal.hide()" data-dismiss="modal">Cancel</button>
+                      <button type="submit" class="btn opt">Add</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
             
           </center>
           
@@ -36,17 +66,19 @@
 </template>
 <script>
 import axios from 'axios'
-
+import {Modal} from 'bootstrap';
 export default {
     title:'Seller Profile',
     name:'SellerProfile',
     data(){
       return{
+        modal:null,
         seller:{},
         user:{},
       }
     },
     async mounted(){
+     this.modal=new Modal(this.$refs.modal);
      const seller = await axios.get("http://localhost:3000/getSeller/"+localStorage.getItem('userid')) 
      this.seller = seller.data
      this.user = seller.data.users
@@ -61,6 +93,12 @@ export default {
     @import '../../assets/sass/maxin';
     @import'../../../node_modules/bootstrap/scss/bootstrap.scss';
     @import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
+    // @import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css";
+
+#profilepic{
+  background-color: #d60265;
+  color: #d60265;
+}
 .header {
   color: #d60265;
 }
@@ -119,7 +157,7 @@ export default {
 .data{
     color: black;
 }
-.btn{
+.pink{
     background-color: #d60265;
     color: white;
 }
