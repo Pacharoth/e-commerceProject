6dadd75fbb34c8fe9efd192a9fcce434ef6294ed
kongkong88-cryto <unlykong@yahoo.com>
2021-07-07 11:11:40 +0700

    [Fixed] Data

--- frontend/src/components/customer/receipt.vue
+++ frontend/src/components/customer/receipt.vue
@@ -73,7 +73,7 @@ export default {
         });
         function generatePDF(){
            var pdf = new jsPDF('p','pt','A3');
-           pdf.internal.pageSize.width=765;
+           pdf.internal.pageSize.width=710;
             pdf.html(form.value,{
                 filename:"Receipt_"+orderDate.value
             }).save()
--- frontend/src/hook/feedback.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-import store from "../stores/feedback.js"
-import store from "../utils/FormValidation"
-
-async function getFeedbacks(store){
-    
-}
